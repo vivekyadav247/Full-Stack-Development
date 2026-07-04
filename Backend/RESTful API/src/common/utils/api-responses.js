ce3abc90ff7459284c78939741e83868df001152
vivekyadav247 <vivekyad240706@gmail.com>
2026-07-04 22:04:44 +0530

    Learn to Build Standard ResfulAPI , connect mongoose with atandard production level folder structure and Learn to create base API responses and error with status code and messages with stati function to use anywhere in Backend

---
+++ Backend/RESTful API/src/common/utils/api-responses.js
@@ -0,0 +1,25 @@
+class ApiResponses {
+  static OK(res, data=null, message){
+    return res.status(200).json({
+      status: "success",
+      data,
+      message
+    })
+  }
+
+  static created(res, data=null, message){
+    return res.status(201).json({
+      status: "success",
+      data,
+      message
+    })
+  }
+
+  static deleted(res, data=null, message){
+    return res.status(200).json({
+      status: "success",
+      data,
+      message
+    })
+  }
+}
