8735e512907d6e1bf6a9c8666f07435cebafbe0f
Vivek Yadav <vivekyad240706@gmail.com>
2026-07-29 22:43:54 +0530

    Transactions

---
+++ Backend/SQL/09-transactions.sql
@@ -0,0 +1,1 @@
+// Transactions in SQL are used to ensure that a series of operations are executed as a single unit of work. This means that either all operations within the transaction are completed successfully, or none of them are applied, maintaining the integrity of the database.
