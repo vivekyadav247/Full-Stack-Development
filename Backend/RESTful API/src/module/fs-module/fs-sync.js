fb2c3fda2d22c8bcac6bc4b87e1207eba7c9bb8e
vivekyadav247 <vivekyad240706@gmail.com>
2026-08-21 19:55:52 +0530

    How to write, read , append and Create folder using fs module

---
+++ Backend/RESTful API/src/module/fs-module/fs-sync.js
@@ -0,0 +1,22 @@
+import fs from "node:fs" ;
+import { encode } from "node:punycode";
+
+// 1. write 
+
+// fs.writeFileSync("test.txt", "Hello From Sync FS ! 2")
+
+// 2. Read 
+
+// const data = fs.readFileSync("text.txt", "utf-8")
+// console.log(data) 
+
+// 3. Update
+
+// // append
+// fs.appendFileSync("test.txt", "\nKyaa Haal Hai")
+
+
+
+// Creating folder
+// fs.mkdirSync("myFolder/innerFolder", {recursive: true}) ;
+
