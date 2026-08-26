1fa3e44fc9ec630068a899d119116835257d4693
vivekyadav247 <vivekyad240706@gmail.com>
2026-08-26 17:48:39 +0530

    Multer File Storage and Operation Completed

--- Backend/RESTful API/src/app.js
+++ Backend/RESTful API/src/app.js
@@ -22,27 +22,38 @@ app.use(express.urlencoded({extended: true})) ;
 app.use(cookieParser()) ;
 
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, path.join(__dirname, '../public/upload'))
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//     const ext = path.extname(file.originalname) ;
-//     cb(null, file.fieldname + '-' + uniqueSuffix + ext)
-//   }
-// })
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '../public/uploads'))
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    const ext = path.extname(file.originalname) ;
+    cb(null, file.fieldname + '-' + uniqueSuffix + ext)
+  }
+})
 
-const storage = multer.memoryStorage() ;
+// const storage = multer.memoryStorage() ;
 
 
 
-const upload = multer({storage});
+const upload = multer({
+    storage,
+    limits:{
+        fileSize: 1024 * 1024 * 5 // 5mb
+    },
+    fileFilter: (req, file, cb) => {
+        const allowed = ["image/jpeg", "image/png", "image/jpg"] ;
 
-app.post("/upload", upload.single("file"), (req, res) => {
-    console.log(req.file.buffer) ;
+        if(!allowed.includes(file.mimetype)){
+            return cb(new ApiError(400, "Only .jpg, .jpeg and .png format allowed!"), false) ;
+        }
+    }
+});
 
-    ApiResponse.ok(res, req.file, "File Uploaded Successfully") ;
+app.post("/upload", upload.array("photos"), (req, res) => {
+    console.log(req.files.buffer) ;
+    ApiResponse.ok(res, req.files, "File Uploaded Successfully") ;
 })
 
 
